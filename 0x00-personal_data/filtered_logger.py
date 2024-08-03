551c12461e5993822275b0a4fd086382a0a04661
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-03 13:40:57 +0100

    Task 1 solution

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 """This module implements personal data security"""
+import logging
 import re
-from typing import List
+from typing import List, Tuple
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.getMessage()
+        filtered_message = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        record.msg = filtered_message
+        return super(RedactingFormatter, self).format(record)
 
 
 def filter_datum(fields: List[str], redaction: str,
