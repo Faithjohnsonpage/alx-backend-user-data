9e01a2df0b33df093a1f0e77d24ba172c6ea2dd1
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-03 13:50:35 +0100

    Task 1 solution

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,14 +11,13 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: Tuple[str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         message = record.getMessage()
-        filtered_message = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
-        record.msg = filtered_message
+        record.msg = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
 
