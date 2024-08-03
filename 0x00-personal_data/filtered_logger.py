3fa6154fffb0988d058aff36be6c884cf950e2f7
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-03 22:58:50 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,7 +61,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """This function returns a connector to the database"""
     connection = mysql.connector.connect(
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
