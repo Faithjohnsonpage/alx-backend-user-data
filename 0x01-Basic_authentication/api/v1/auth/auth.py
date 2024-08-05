b9d8fce3f1ed7b35f2b7597c4682b6b802fa6bfe
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-05 23:18:56 +0100

    Task 3 solution

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """This class manages the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if the path requires authentication"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Get the authorization header from the request"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current user from the request"""
+        return None
