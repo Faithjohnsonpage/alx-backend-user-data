a17641621753c1d94775eb437fc43c54e7a3814b
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-13 12:12:26 +0100

    Task 8 solution

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,6 +25,15 @@ class Auth:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks the validity of user login details"""
+        try:
+            user = self._db.find_user_by(email=email)
+            # Check the password directly against the stored hash
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Hash the password"""
