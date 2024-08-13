75146202fc710095345fbc4f2b2f5e9354e5499a
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-13 21:37:21 +0100

    Task 16 solution

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,6 +62,16 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token_id = _generate_uuid()
+            user.reset_token = reset_token_id
+            return reset_token_id
+        except NoResultFound:
+            raise ValueError
+
 
 def _generate_uuid() -> str:
     """Generates a string representation of a UUID"""
