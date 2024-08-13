be910e1a7880c5c0552992eba4cbdee98113090f
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-13 22:01:35 +0100

    Task 17 solution

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,5 +70,20 @@ def profile() -> str:
     return response
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> dict:
+    """Get reset password token"""
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        json_payload = {"email": email, "reset_token": reset_token},
+        response = make_response(jsonify(json_payload, 200)
+        return response
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
