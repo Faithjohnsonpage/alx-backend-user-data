6c44b661d95c56830887090e1ad7fb86feb96d1f
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-14 09:57:01 +0100

    Task 19 solution

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,7 +84,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
-def update_password() -> str:
+def update_password() -> dict:
     """Update password"""
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
