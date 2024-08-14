8e4ad89e312b1d6b98a86d54f05986dc338acf43
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-14 06:26:31 +0100

    Task 18 solution

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,6 +72,16 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            user.hashed_password = hashed_password
+            user.reset_token = None
+        except NoResultFound:
+            raise ValueError
+
 
 def _generate_uuid() -> str:
     """Generates a string representation of a UUID"""
